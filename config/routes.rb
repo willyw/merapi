221946017825cbf66907833e3d2c821cc0f327f1
“Willy <will@rubify.com>
2010-11-10 22:45:38 +0800

    about us

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => "welcome", :action => "index"
+  map.about "/about_us" , :controller =>"welcome", :action => "about"
   
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
