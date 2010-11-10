3b52d66e641046e481c89f10e9faa604d4b0fb91
“Willy <will@rubify.com>
2010-11-10 20:29:12 +0800

    options in

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -35,8 +35,12 @@ $(document).ready(function(){
 		
 		
 		var options ={
+				'transitionIn'	:	'elastic',
+				'transitionOut'	:	'elastic',
+				'speedIn'		:	600, 
+				'speedOut'		:	200, 
+				'overlayShow'	:	false
 		};
-		
 		
 		$.fancybox(arrayOfImages,options);
 		return false;
