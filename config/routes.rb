7992b38e72500efbef5a9b3c43de82c3c63a1477
“Willy <will@rubify.com>
2010-11-22 23:22:48 +0800

    halfway

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => "welcome", :action => "index"
-  map.about "/donate" , :controller =>"welcome", :action => "donate"
+  map.donate "/donate" , :controller =>"welcome", :action => "donate"
   
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
